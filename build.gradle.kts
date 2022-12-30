4a5d5e86deb9d30cb46cc12e4d7829f4b88affe6
Julian Goede <julian.goede@pm.me>
2022-12-30 15:44:08 +0100

    updated to spring v3 todo: - native tests are not running - jpms jlink and pack - java 19 virtual threads

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,15 +2,15 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     // Kotlin
-    val kotlinVersion = "1.7.21"
+    val kotlinVersion = "1.7.22"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.jpa") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
 
     // Spring
-    id("org.springframework.boot") version "3.0.0"
+    id("org.springframework.boot") version "3.0.1"
     id("io.spring.dependency-management") version "1.1.0"
-    id("org.graalvm.buildtools.native") version "0.9.17"
+    id("org.graalvm.buildtools.native") version "0.9.19"
 }
 
 group = "de.cmdjulian"
@@ -28,6 +28,7 @@ repositories {
 
 dependencies {
     // Spring
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-hateoas")
     implementation("org.springframework.boot:spring-boot-starter-validation")
@@ -60,39 +61,59 @@ dependencies {
     testImplementation("io.kotest:kotest-assertions-core-jvm:5.5.4")
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf(
-            "-Xjsr305=strict",
-            "-Xemit-jvm-type-annotations",
-            "-java-parameters",
-            "-Xjvm-default=all",
-            "-Xcontext-receivers"
-        )
-        jvmTarget = "${JavaVersion.VERSION_17}"
+tasks.register("bootRunWithNativeImageAgent") {
+    group = "application"
+    description = "Runs bootRun with GraalVM tracing agent enabled"
+    doFirst {
+        tasks.bootRun {
+            jvmArgs!!.plusAssign("-agentlib:native-image-agent=config-output-dir=native-image")
+        }
     }
+    finalizedBy("bootRun")
 }
 
-graalvmNative {
-    toolchainDetection.set(false)
-    binaries {
-        all {
-            resources.autodetect()
-        }
-        named("main") {
-            imageName.set("notes")
-            buildArgs("--static", "--libc=musl", "--enable-url-protocols=http,https")
+tasks.register("nativeCompileStatic") {
+    group = "application"
+    description = "executes the graalvm native image build and produce a statically linked native image binary"
+    doFirst {
+        graalvmNative {
+            binaries {
+                named("main") {
+                    buildArgs("--static", "--libc=musl")
+                }
+            }
         }
     }
-    metadataRepository {
-        enabled.set(true)
-        version.set("0.2.5")
+    finalizedBy("nativeCompile")
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions {
+        javaParameters = true
+        freeCompilerArgs += listOf("-Xjsr305=strict", "-Xemit-jvm-type-annotations", "-java-parameters", "-Xjvm-default=all", "-Xcontext-receivers")
+        jvmTarget = "${JavaVersion.VERSION_17}"
     }
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
 }
+
+graalvmNative {
+    agent {
+        defaultMode.set("standard")
+    }
+    toolchainDetection.set(false)
+    binaries {
+        all {
+            resources.autodetect()
+        }
+    }
+    metadataRepository {
+        enabled.set(true)
+    }
+}
+
 
 tasks {
     bootBuildImage {
